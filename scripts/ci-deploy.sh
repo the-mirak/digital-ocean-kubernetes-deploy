b6818ff8ea471a502ce58fdfe97ab7b4c144f07a
the-mirak <k.benzidane@gmail.com>
2021-09-06 19:58:16 +0100

    echoing

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -14,11 +14,15 @@ envsubst <./kube/do-sample-deployment.yml >./kube/do-sample-deployment.yml.out
 mv ./kube/do-sample-deployment.yml.out ./kube/do-sample-deployment.yml
 
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+
 cat cert.crt
 
-echo $KUBERNETES_SERVER
+echo "----------------"
+echo "$KUBERNETES_SERVER"
+echo "----------------"
 
-echo $KUBERNETES_TOKEN
+
+echo "$KUBERNETES_TOKEN"
 
 ./kubectl \
   --kubeconfig=/dev/null \
